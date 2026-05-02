70b255c11ca946173fb89f662a4f9e780bfcb794
milon <milonmondolmd33@gmail.com>
2026-05-02 12:52:27 +0600

    updateee

--- src/components/ui/DashboardNav.jsx
+++ src/components/ui/DashboardNav.jsx
@@ -50,7 +50,7 @@ function DashboardNav() {
           />
 
           {/* RIGHT → Menu Icon (NO BG) */}
-          <div className="text-pink-600 text-2xl cursor-pointer mr-1">
+          <div className="text-whi text-2xl cursor-pointer mr-1">
             {menuOpen ? (
               <FaTimes onClick={() => setMenuOpen(false)} />
             ) : (
